f8c58f8116eb40042597176841a8db448efd884d
rainwont <rainwont@gmail.com>
2015-05-12 12:00:09 +0900

    Hello World

--- sampleapp/config/routes.rb
+++ sampleapp/config/routes.rb
@@ -1,4 +1,5 @@
 Sampleapp::Application.routes.draw do
+  get "hello/index"
   get "top/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
