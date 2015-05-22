3722246b44d28f50dae9b3b9bdf2ea1292d5a218
rainwont <rainwont@gmail.com>
2015-05-22 16:36:08 +0900

    セッションを使ったログイン機能追加
    正常に動く

--- sampleapp/config/routes.rb
+++ sampleapp/config/routes.rb
@@ -13,6 +13,9 @@ Sampleapp::Application.routes.draw do
   resources :members do
     collection { get "search"}
   end
+
+  resource :session, only: [:create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
