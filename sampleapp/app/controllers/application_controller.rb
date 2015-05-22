c8cf0f9f05493b6e40615d386593ac2860a88afd
rainwont <rainwont@gmail.com>
2015-05-22 16:50:44 +0900

    セッションを使ったログイン機能追加の修正

--- sampleapp/app/controllers/application_controller.rb
+++ sampleapp/app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery
+
+  before_filter :authorize
+
+  class Forbidden < StandardError; end
+
+  private
+  def authorize
+    if session[:member_id]
+      @current_member = Member.find_by_id(session[:member_id])
+      session.delete(:member_id) unless @current_member
+    end
+  end
+
+  def login_required
+    raise Forbidden unless @current_member
+  end
 end
