6256c339e1a356bd9a25201c9b473da7c0efd308
rainwont <rainwont@gmail.com>
2015-05-27 16:58:46 +0900

    会員情報の会員に画像を表示機能を追加

--- sampleapp/app/models/member_image.rb
+++ sampleapp/app/models/member_image.rb
@@ -1,3 +1,10 @@
 class MemberImage < ActiveRecord::Base
   belongs_to :member
+
+  IMAGE_TYPES = { "image/jpeg" => "jpg", "image/gif" => "gif",
+                  "image/png" => "png" }
+  def extension
+    IMAGE_TYPES[content_type]
+  end
+
 end
